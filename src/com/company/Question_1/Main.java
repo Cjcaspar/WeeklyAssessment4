89e39a554096047db2bb2628e5e0383437f67e36
Conner Caspar <conner.caspar@gmail.com>
2018-02-16 14:48:53 -0500

    Q1 answered

--- src/com/company/Question_1/Main.java
+++ src/com/company/Question_1/Main.java
@@ -1,9 +1,13 @@
 package com.company.Question_1;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
    /*
     YOUR NAME HERE! - No really put your name here so we know who this belongs to when we grade it <3
+
+    Conner Caspar
      */
 
     public static void main(String[] args) {
@@ -14,6 +18,26 @@ public class Main {
         4. Have this new method use a ForEach loop to iterate through the ArrayList and display each item to the user.
          */
 
+        List<String> library = new ArrayList<>();
+        addToArrayList(library);
+        displayArray(library);
 
+
+    }
+
+    public static void addToArrayList(List<String> library) {
+
+        library.add("Call of Duty");
+        library.add("PuBG");
+        library.add("Hearthstone");
+        library.add("League of Legends");
+        library.add("WoW");
+        library.add("CSGO");
+    }
+
+    public static void displayArray(List<String> library) {
+        for (String game : library) {
+            System.out.println(game);
+        }
     }
 }
