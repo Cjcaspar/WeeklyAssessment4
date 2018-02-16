f9d74d4ab9d0fabc6b34e046b8f3dfec59c76711
Conner Caspar <conner.caspar@gmail.com>
2018-02-16 14:51:17 -0500

    Answered question 2

---
+++ src/com/company/Question_2/House.java
@@ -0,0 +1,27 @@
+package com.company.Question_2;
+
+public class House {
+    private int bedrooms;
+    private int bathrooms;
+
+    public House(int bedrooms, int bathrooms) {
+        this.bedrooms = bedrooms;
+        this.bathrooms = bathrooms;
+    }
+
+    public int getBedrooms() {
+        return bedrooms;
+    }
+
+    public void setBedrooms(int bedrooms) {
+        this.bedrooms = bedrooms;
+    }
+
+    public int getBathrooms() {
+        return bathrooms;
+    }
+
+    public void setBathrooms(int bathrooms) {
+        this.bathrooms = bathrooms;
+    }
+}
